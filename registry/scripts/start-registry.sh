6449680854a14f952b188d4e58f9bcad7a710a6b
Anton Galitsyn <anton.galitsyn@gmail.com>
2016-05-07 15:57:18 +0600

    #5 move to tabs

--- registry/scripts/start-registry.sh
+++ registry/scripts/start-registry.sh
@@ -9,15 +9,15 @@ kubectl create -f $SCRIPT_DIR/../k8s/replication-controller.yaml
 echo -n 'Waiting for pod (Interrupt with Control-C)'
 
 while true; do
-    echo -n '.'
-    POD=$(kubectl get pods --namespace=kube-system -l k8s-app=kube-registry \
-            -o template --template '{{range .items}}{{.metadata.name}} {{.status.phase}}{{"\n"}}{{end}}' \
-            | grep Running | head -1 | cut -f1 -d' ')
-    if [[ -n "$POD" ]]; then
-        echo "Got pod $POD"
-        break
-    fi
-    sleep 1
+	echo -n '.'
+	POD=$(kubectl get pods --namespace=kube-system -l k8s-app=kube-registry \
+			-o template --template '{{range .items}}{{.metadata.name}} {{.status.phase}}{{"\n"}}{{end}}' \
+			| grep Running | head -1 | cut -f1 -d' ')
+	if [[ -n "$POD" ]]; then
+		echo "Got pod $POD"
+		break
+	fi
+	sleep 1
 done
 
 kubectl port-forward --namespace=kube-system $POD 5000:5000 2>&1 | logger &
