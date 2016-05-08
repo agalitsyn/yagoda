a8f3356baf2d2e626cca7e193a09a27d51f21e5a
Alexey Medvedchikov <alexey.medvedchikov@gmail.com>
2016-05-09 02:44:00 +0600

    Vagrant now 4 VMS, using controller as worker too, lowering resource limits

--- vagrant/config.rb
+++ vagrant/config.rb
@@ -1,11 +1,11 @@
 $update_channel="alpha"
 
 $controller_count=1
-$controller_vm_memory=512
+$controller_vm_memory=1024
 
-$worker_count=3
-$worker_vm_memory=2048
+$worker_count=2
+$worker_vm_memory=1024
 
 $etcd_count=1
-$etcd_vm_memory=512
+$etcd_vm_memory=256
 
