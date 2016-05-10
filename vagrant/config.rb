293c33344afc11ae3f36a61f3b49856c1c4c5264
Alexey Medvedchikov <alexey.medvedchikov@gmail.com>
2016-05-10 21:10:21 +0600

    Memory enlarge

--- vagrant/config.rb
+++ vagrant/config.rb
@@ -1,10 +1,10 @@
 $update_channel="stable"
 
 $controller_count=1
-$controller_vm_memory=1024
+$controller_vm_memory=2048
 $controller_vcpus=2
 
 $worker_count=2
-$worker_vm_memory=1024
+$worker_vm_memory=2048
 $worker_vcpus=2
 
